d8e16924647b353f689f41fc3ae57881cde3cff1
ysard <ysard@users.noreply.github.com>
2025-08-10 17:20:47 +0200

    [58xx][5838] Update specific i2s driver; add generic init func if AAD is not enabled at config time

--- t5838_drv.c
+++ t5838_drv.c
@@ -6,21 +6,13 @@
  * @copyright (c) 2025 Ysard. All rights reserved.
  */
 
+#include "esp_log.h"
 #include "t5838.h"
 
-static const char *TAG = "T5838_I2S";
+static const char *TAG = "T5838_PDM";
 
-/**
- * @brief Init the T5838 PDM device & prepare AAD settings.
- *
- * @param[in] pdm_rx_cfg I2S PDM configuration.
- * @param[in] chan_cfg Pointer to channel configuration, can be left to NULL automatically select the I2S port.
- * @param[in] t5838_aad_cfg Pointer to AAD pinout configuration.
- * @param[out] dev Pointer to device structure which will be populated, ready to be used for AAD configurations.
- * @param[out] rx_handle Pointer to the channel handle, which will be populated.
- */
-esp_err_t t5838_init(const i2s_pdm_rx_config_t *pdm_rx_cfg, i2s_chan_config_t *chan_cfg, struct t5838_aad_drv_cfg *t5838_aad_cfg,
-                     struct device *dev, i2s_chan_handle_t *rx_handle) {
+
+esp_err_t pdm_init(const i2s_pdm_rx_config_t *pdm_rx_cfg, i2s_chan_config_t *chan_cfg, i2s_chan_handle_t *rx_handle) {
     esp_err_t err;
 
     /* Allocate an I2S RX channel */
@@ -41,26 +33,45 @@ esp_err_t t5838_init(const i2s_pdm_rx_config_t *pdm_rx_cfg, i2s_chan_config_t *c
         ESP_LOGE(TAG, "i2s_channel_init_pdm_rx_mode failed: %d", err);
         return err;
     }
+}
+
+
+/**
+ * @brief Init the T5838 PDM device & prepare AAD settings.
+ *
+ * @param[in] pdm_rx_cfg I2S PDM configuration.
+ * @param[in] chan_cfg Pointer to channel configuration, can be left to NULL to
+ *  automatically select the I2S port.
+ * @param[in] t58xx_aad_cfg Pointer to AAD pinout configuration.
+ * @param[out] aad_dev Pointer to device structure which will be populated,
+ *  ready to be used for AAD configurations.
+ * @param[out] rx_handle Pointer to the channel handle, which will be populated.
+ */
+esp_err_t t5838_init(const i2s_pdm_rx_config_t *pdm_rx_cfg, i2s_chan_config_t *chan_cfg, struct t58xx_aad_drv_cfg *t58xx_aad_cfg,
+                     struct device *aad_dev, i2s_chan_handle_t *rx_handle) {
+    esp_err_t err = pdm_init(pdm_rx_cfg, chan_cfg, rx_handle);
+    if (err != ESP_OK)
+        return err;
 
     /* Build the device structure for AAD configuration */
-    struct t5838_drv_data t5838_data = {
+    struct t58xx_drv_data t58xx_data = {
         .rx_handle = *rx_handle
     };
-    struct device t5838_pdm_dev = {
+    struct device t58xx_pdm_dev = {
         .config = pdm_rx_cfg,
-        .data = &t5838_data
+        .data = &t58xx_data
     };
 
     /* Inject PDM device in the AAD config */
     // (Used later to test if the PDM clock is released,
     // and ready to be used for AAD config through THSEL pin)
-    t5838_aad_cfg->pdm_dev = &t5838_pdm_dev;
+    t58xx_aad_cfg->i2s_dev = &t58xx_pdm_dev;
 
-    struct t5838_aad_drv_data t5838_aad_data;
+    struct t58xx_aad_drv_data t58xx_aad_data;
 
-    *dev = (struct device) {
-        .config = t5838_aad_cfg,
-        .data = &t5838_aad_data
+    *aad_dev = (struct device) {
+        .config = t58xx_aad_cfg,
+        .data = &t58xx_aad_data
     };
     return ESP_OK;
 }
