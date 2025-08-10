cc5e47bad4d67b735b3e3f80355219777b9ac07b
ysard <ysard@users.noreply.github.com>
2025-08-10 17:21:16 +0200

    [58xx][5848] Update specific i2s driver

---
+++ t5848_drv.c
@@ -0,0 +1,77 @@
+/** @file t5848_drv.c
+ *
+ * @brief Functions related to the configuration of the I2S driver
+ *  for the T5848 MEMS microphone by TDK.
+ *
+ * @copyright (c) 2025 Ysard. All rights reserved.
+ */
+
+#include "esp_log.h"
+#include "t5848.h"
+
+static const char *TAG = "T5848_I2S";
+
+
+esp_err_t i2s_init(const i2s_std_config_t *std_rx_cfg, i2s_chan_config_t *chan_cfg, i2s_chan_handle_t *rx_handle) {
+    esp_err_t err;
+
+    /* Allocate an I2S RX channel */
+    if (chan_cfg == NULL) {
+        /* With I2S_NUM_AUTO rx channel will be registered on another I2S,
+         * if no other available I2S unit found it will return ESP_ERR_NOT_FOUND */
+        *chan_cfg = (i2s_chan_config_t) I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
+    }
+    err = i2s_new_channel(chan_cfg, NULL, rx_handle);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "i2s_new_channel failed: %d", err);
+        return err;
+    }
+
+    /* Init the channel into I2S RX mode */
+    err = i2s_channel_init_std_mode(*rx_handle, std_rx_cfg);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "i2s_channel_init_std_mode failed: %d", err);
+        return err;
+    }
+}
+
+
+/**
+ * @brief Init the T5848 I2S device & prepare AAD settings.
+ *
+ * @param[in] std_rx_cfg I2S STD configuration.
+ * @param[in] chan_cfg Pointer to channel configuration, can be left to NULL to
+ *  automatically select the I2S port.
+ * @param[in] t58xx_aad_cfg Pointer to AAD pinout configuration.
+ * @param[out] aad_dev Pointer to device structure which will be populated,
+ *  ready to be used for AAD configurations.
+ * @param[out] rx_handle Pointer to the channel handle, which will be populated.
+ */
+esp_err_t t5848_init(const i2s_std_config_t *std_rx_cfg, i2s_chan_config_t *chan_cfg, struct t58xx_aad_drv_cfg *t58xx_aad_cfg,
+                     struct device *aad_dev, i2s_chan_handle_t *rx_handle) {
+    esp_err_t err = i2s_init(std_rx_cfg, chan_cfg, rx_handle);
+    if (err != ESP_OK)
+        return err;
+
+    /* Build the device structure for AAD configuration */
+    struct t58xx_drv_data t58xx_data = {
+        .rx_handle = *rx_handle
+    };
+    struct device t58xx_std_dev = {
+        .config = std_rx_cfg,
+        .data = &t58xx_data
+    };
+
+    /* Inject I2S device in the AAD config */
+    // (Used later to test if the I2S clock is released,
+    // and ready to be used for AAD config through THSEL pin)
+    t58xx_aad_cfg->i2s_dev = &t58xx_std_dev;
+
+    struct t58xx_aad_drv_data t58xx_aad_data;
+
+    *aad_dev = (struct device) {
+        .config = t58xx_aad_cfg,
+        .data = &t58xx_aad_data
+    };
+    return ESP_OK;
+}
